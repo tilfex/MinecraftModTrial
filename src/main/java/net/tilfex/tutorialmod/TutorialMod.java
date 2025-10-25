0b64f5aaf55d096a0576f57953d7ab6cb932bdde
tilfex <felix.trinler@mail.de>
2025-10-25 11:15:45 +0200

    init

---
+++ src/main/java/net/tilfex/tutorialmod/TutorialMod.java
@@ -0,0 +1,19 @@
+package net.tilfex.tutorialmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TutorialMod implements ModInitializer {
+
+    //    save mod ID in a variable
+	public static final String MOD_ID = "tutorialmod";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
